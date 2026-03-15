2c35d8b86e9d4413b11e6bb2668a8a7491f34510
Kawtar <Kawtar.shaimi8@gmail.com>
2026-03-15 22:49:11 +0000

    Modify FitMeAiApplication.java

---
+++ backend/src/main/java/com/fitmeai/FitMeAiApplication.java
@@ -0,0 +1,12 @@
+package com.fitmeai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitMeAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitMeAiApplication.class, args);
+    }
+}
